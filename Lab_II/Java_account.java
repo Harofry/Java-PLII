434a15a1929788e59541cacdffa88cc335c67295
Gabriel Giulian <gabriel.harofry@gmail.com>
2025-01-20 23:43:16 -0300

    Update Java_account.java

--- Lab_II/Java_account.java
+++ Lab_II/Java_account.java
@@ -13,3 +13,25 @@ public abstract class Conta implements Serializable {
     public String getNumero() {
         return numero;
     }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Erro, valor do depósito deve ser positivo.");
+        }
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Erro, o valor do saque deve ser positivo.");
+        }
+        if (valor > saldo) {
+            throw new IllegalArgumentException("Alerta! Saldo insuficiente.");
+        }
+        saldo -= valor;
+    }
+
